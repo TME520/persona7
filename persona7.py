cecbdaf62ccf0b3401eec338e47aebe9545e5cd4
Five Twenty <tme520@gmail.com>
2020-08-24 01:14:28 +1000

    Starting work on the conversation engine

--- persona7.py
+++ persona7.py
@@ -15,7 +15,6 @@ from joblib import load
 from argparse import ArgumentParser
 import boto3
 from colorama import Fore, Style, init
-import threading
 
 init(autoreset=True)
 
@@ -24,7 +23,7 @@ nltk.download('stopwords')
 nltk.download('wordnet')
 
 publicname='Persona/7'
-version='0.10'
+version='0.11'
 
 # instantiate Slack client
 slack_client = RTMClient(token=os.environ.get('SLACKTOKEN'))
@@ -43,8 +42,31 @@ RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = 'do'
 MENTION_REGEX = '^<@(|[WU].+?)>(.*)'
 
-def testMultithread(argument):
-    print(threading.currentThread().getName(), f'Running with argument {argument}...')
+def cleanupIncDesc(pathToCB1File):
+    print(Fore.LIGHTGREEN_EX + 'cleanupIncDesc')
+    stopWords = set(stopwords.words('english'))
+    crapWords = [';', ':', '-', '.', ',', '(', ')', '[', ']', '&', '--', '#']
+    wordnet_lemmatizer = WordNetLemmatizer()
+    wordsFiltered = []
+
+    currentCB1File = open(pathToCB1File, 'r')
+    currentDescription = currentCB1File.readlines()
+    currentCB1File.close()
+    for g in currentDescription:
+        currentCB1File = open(pathToCB1File + '.processed', 'w')
+        currentCB1File.write('--- FILTERED DATA ---\n')
+        # Remove empty lines + crap characters
+        if g != '' and g!='\n':
+            tokens = nltk.word_tokenize(g)
+            for w in tokens:
+                if (w.lower() not in stopWords) and (w.lower() not in crapWords):
+                    word_lemme = wordnet_lemmatizer.lemmatize(w.lower())
+                    wordsFiltered.append(word_lemme)
+    currentCB1File.write(str(wordsFiltered))
+    currentCB1File.write('\n--- *** ---')
+    currentCB1File.close()
+    print('Cleaned up description:\n')
+    print(str(wordsFiltered))
 
 def publishToSlack(msg, chan, icon, creds):
     try:
